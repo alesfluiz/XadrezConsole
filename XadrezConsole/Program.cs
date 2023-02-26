5f2b89f98ba1ee375c1aa997bc3113921e615331
unknown <alesfluiz@gmail.com>
2023-02-26 15:53:13 -0300

    Controlando as pecas capturadas

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -19,10 +19,7 @@ namespace XadrezConsole
                     try
                     {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.Tab);
-                        Console.WriteLine();
-                        Console.WriteLine($"Turno: {partida.Turno}");
-                        Console.WriteLine($"Aguardando Jogada: {partida.JogadorAtual}");
+                        Tela.ImprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
